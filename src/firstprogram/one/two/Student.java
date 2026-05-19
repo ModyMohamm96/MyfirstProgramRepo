2a1179d54d3d740aaf3ad568b83b2daaad8e5c27
ModyMohamm96 <mohamm96@sheridancollege.ca>
2026-05-19 14:14:04 -0400

    my first program

---
+++ src/firstprogram/one/two/Student.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package firstprogram.one.two;
+
+/**
+ *
+ * @author Mohammed Mohammed
+ */
+public class Student {
+    int id;
+    String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
